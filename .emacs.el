0e3de0315a380034b077b7be5f9f6e9adba4e71d
Nagi <nagi@nagi.com>
2016-04-17 03:24:16 +0200

    added many new configs from abrahams dot-file

--- .emacs.el
+++ .emacs.el
@@ -246,6 +246,17 @@
 ;; Pas-de-dialog-gtk
 (setq use-file-dialog nil)
 
+;;; Powerline
+;; testing
+;; (use-package powerline
+;;        :ensure t)
+;; (custom-set-faces
+;;      '(mode-line-buffer-id ((t (:foreground "#000000" :bold t))))
+;;      '(which-func ((t (:foreground "#77aaff"))))
+;;      '(mode-line ((t (:foreground "#000000" :background "#dddddd" :box nil))))
+;;      '(mode-line-inactive ((t (:foreground "#000000" :background "#bbbbbb" :box nil)))))
+
+
 ;; Browse url
 ;;
 ;;
@@ -566,13 +577,6 @@
   :bind
   ("C-=" . er/expand-region))
 
-(use-package multiple-cursors
-  :ensure t
-  :config
-  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
-
 ;;; W3m
 ;;
 (use-package w3m
@@ -608,6 +612,7 @@
     (setq magit-revision-show-gravatars nil)
     (setq magit-uniquify-buffer-names nil))
   :config
+  (global-set-key (kbd "C-x g") 'magit-status)
   (bind-key "C" 'magit-commit-add-log magit-diff-mode-map)
   (bind-key "C-]" 'magit-toggle-margin magit-log-mode-map)
   :no-require t
@@ -703,9 +708,14 @@
                                     (let ((default-directory (getenv "HOME")))
                                       (command-execute 'eshell)
                                       (bury-buffer)))))
+(setq eshell-scroll-to-bottom-on-input t)
+;; *Note:* hitting return on any other line will copy that line to the
+;;     prompt and immediately execute it.
+
+
 
 ;;; Projectile
-;; http://tuhdo.github.io/helm-projectile.html 
+;; http://tuhdo.github.io/helm-projectile.html
 ;;
 (use-package projectile
   :load-path "site-lisp/projectile"
@@ -721,7 +731,7 @@
     (helm-projectile-on))
   (projectile-global-mode))
 
-(use-package helm-projectile 
+(use-package helm-projectile
   :defer t :ensure t
   :ensure helm-projectile)
 
@@ -730,6 +740,175 @@
 ;;
 (use-package bookmark+ :ensure t)
 
+;;; guide-key
+;;
+(use-package guide-key
+       :ensure t
+       :init    (setq guide-key/guide-key-sequence
+                      '("C-x r"     ; rectanges and registers
+                        "C-x 4"     ; window commands
+                        "M-s h"     ; hi-lock highlighting
+                        "C-x w"     ; alternative to M-s ...
+                        "C-c @"     ; hs-hide-show mode
+                        "C-c p"     ; projectile
+                        "<f2>"
+                        "<f9>"
+                        (org-mode "C-c C-x")))
+       :config  (guide-key-mode 1)
+       :diminish guide-key-mode)
+(setq guide-key/guide-key-sequence '("C-x"))
+(setq guide-key/recursive-key-sequence-flag t)
 
-;; TODO: 
+;;; fancy-narrow
+(use-package fancy-narrow
+  :ensure t
+  :config
+  (defun ha/highlight-block ()
+    "Highlights a 'block' in a buffer defined by the first blank
+     line before and after the current cursor position. Uses the
+          'fancy-narrow' mode to high-light the block."
+    (interactive)
+    (let (cur beg end)
+      (setq cur (point))
+      (setq end (or (re-search-forward  "^\s*$" nil t) (point-max)))
+      (goto-char cur)
+      (setq beg (or (re-search-backward "^\s*$" nil t) (point-min)))
+      (fancy-narrow-to-region beg end)
+      (goto-char cur)))
+
+  (defun ha/highlight-section (num)
+    "If some of the buffer is highlighted with the `fancy-narrow'
+          mode, then un-highlight it by calling `fancy-widen'.
+
+          If region is active, call `fancy-narrow-to-region'.
+
+          If NUM is 0, highlight the current block (delimited by blank
+          lines). If NUM is positive or negative, highlight that number
+          of lines.  Otherwise, called `fancy-narrow-to-defun', to
+          highlight current function."
+    (interactive "p")
+    (cond
+     ((fancy-narrow-active-p)  (fancy-widen))
+     ((region-active-p)        (fancy-narrow-to-region (region-beginning) (region-end)))
+     ((= num 0)                (ha/highlight-block))
+     ((= num 1)                (fancy-narrow-to-defun))
+     (t                        (progn (ha/expand-region num)
+                                      (fancy-narrow-to-region (region-beginning) (region-end))))))
+
+       :bind ("C-M-+" . ha/highlight-section))
+
+;;; endless parentheses
+;;
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+     Intelligently means: region, subtree, or defun, whichever applies
+     first.
+
+     With prefix P, don't widen, just narrow even if buffer is already
+     narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+        (t (narrow-to-defun))))
+(global-set-key (kbd "C-x n x") 'narrow-or-widen-dwim)
+
+;;; ace-window
+;;
+(use-package ace-window
+  :ensure t
+  :init
+  (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l ?o))
+  (global-set-key (kbd "C-x o") 'ace-window)
+  :diminish ace-window-mode)
+
+;;; avy
+;;
+(use-package avy
+  :ensure t
+  :commands avy-goto-word-1 avy-goto-char-1 avy-goto-line avy-goto-char-timer
+  :bind
+  ("C-c j"   . avy-goto-word-1)
+  ("C-c k k" . avy-goto-char-timer)
+  ("C-c k j" . avy-goto-word-1)
+  ("C-c k c" . avy-goto-char-1)
+  ("C-c k l" . avy-goto-line))
+
+;; smarter move
+;; TODO: create emacs-fixes.el
+(defun smarter-move-beginning-of-line (arg)
+    "Move point back to indentation of beginning of line.
+
+  Move point to the first non-whitespace character on this line.
+  If point is already there, move to the beginning of the line.
+  Effectively toggle between the first non-whitespace character and
+  the beginning of the line.
+
+  If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+  point reaches the beginning or end of the buffer, stop there."
+    (interactive "^p")
+    (setq arg (or arg 1))
+
+    ;; Move lines first
+    (when (/= arg 1)
+      (let ((line-move-visual nil))
+        (forward-line (1- arg))))
+
+    (let ((orig-point (point)))
+      (back-to-indentation)
+      (when (= orig-point (point))
+        (move-beginning-of-line 1))))
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
+(global-set-key [remap org-beginning-of-line]  'smarter-move-beginning-of-line)
+
+;;; Hydra
+;;
+(use-package hydra
+  :ensure t
+  :config
+  (hydra-add-font-lock))
+
+;;; Multiple cursors
+;;
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key
+   (kbd "C-c C-.")
+   (defhydra hydra-multiple-cursors ()
+     "multiple-cursors"
+     ("." mc/mark-all-dwim                   "all-dwim")
+     ("C-." mc/mark-all-like-this-dwim       "all-like-dwim")
+     ("n" mc/mark-next-like-this             "next")
+     ("p" mc/mark-previous-like-this         "previous")
+     ("a" mc/mark-all-like-this              "mark-all")
+     ("N" mc/mark-next-symbol-like-this      "next-symbol")
+     ("P" mc/mark-previous-symbol-like-this  "previous-symbol")
+     ("A" mc/mark-all-symbols-like-this      "all-symbols")
+     ("f" mc/mark-all-like-this-in-defun     "in-func")
+     ("l" mc/edit-lines                      "all-lines")
+     ("e" mc/edit-ends-of-lines              "end-lines"))))
+
+
+;;; Dired details
+;;
+(setq ls-lisp-use-insert-directory-program nil)
+(use-package dired-details
+       :ensure t
+       :init   (setq dired-details-hidden-string "* ")
+       :config (dired-details-install))
+
+(use-package find-dired
+        :ensure t
+        :init (setq find-ls-option '("-print0 | xargs -0 ls -od" . "-od")))
+
+(require 'dired-x)
+
+;; TODO group it some where
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; TODO:
 (global-set-key (kbd "C-x C-j") 'dired-jump)
