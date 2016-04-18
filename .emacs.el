c3f50ea29cbc668ef66aef68865356cea606f7b7
Nagi <nagi@nagi.com>
2016-04-19 00:07:52 +0200

    added block wrappers

--- .emacs.el
+++ .emacs.el
@@ -957,6 +957,7 @@
 
 ;;; Yasnippets
 ;;
+;; org-mode : https://github.com/yyr/yasnippets-orgmode.git
 (use-package yasnippet
   :ensure t
   :init
@@ -974,3 +975,108 @@
 (global-set-key (kbd "C-c R")                      #'(lambda () (interactive) (revert-buffer t t)))
 (global-set-key (kbd "C-c W")                      'whitespace-mode)
 (global-set-key (kbd "C-M-j")                      #'(lambda () (interactive) (kill-sexp -1)))
+
+
+;;; Block Wrappers
+;;
+;; While the =M-(= binding to =insert-pair= is great, I often need to
+;; wrap with other characters:
+(global-set-key (kbd "M-[") 'insert-pair)
+(global-set-key (kbd "M-{") 'insert-pair)
+;;(global-set-key (kbd "M-<") 'insert-pair) ;; conflict with go to start
+(global-set-key (kbd "M-'") 'insert-pair)
+(global-set-key (kbd "M-`") 'insert-pair)
+(global-set-key (kbd "M-\"") 'insert-pair)
+
+;;
+;; But [[https://github.com/rejeep/wrap-region.el][wrap-region]] is even more flexible.
+;; NOTE: just select region and press any wrapping char
+(use-package wrap-region
+  :ensure   t
+  :config
+  (wrap-region-global-mode t)
+  (wrap-region-add-wrappers
+   '(("(" ")")
+     ("[" "]")
+     ("{" "}")
+     ("<" ">")
+     ("'" "'")
+     ("\"" "\"")
+     ("‘" "’"   "q")
+     ("“" "”"   "Q")
+     ("*" "*"   "b"   org-mode)                 ; bolden
+     ("*" "*"   "*"   org-mode)                 ; bolden
+     ("/" "/"   "i"   org-mode)                 ; italics
+     ("/" "/"   "/"   org-mode)                 ; italics
+     ("~" "~"   "c"   org-mode)                 ; code
+     ("~" "~"   "~"   org-mode)                 ; code
+     ("=" "="   "v"   org-mode)                 ; verbatim
+     ("=" "="   "="   org-mode)                 ; verbatim
+     ("_" "_"   "u" '(org-mode markdown-mode))  ; underline
+     ("**" "**" "b"   markdown-mode)            ; bolden
+     ("*" "*"   "i"   markdown-mode)            ; italics
+     ("`" "`"   "c" '(markdown-mode ruby-mode)) ; code
+     ("`" "'"   "c"   lisp-mode)                ; code
+     ))
+  :diminish wrap-region-mode)
+
+
+(defun surround (start end txt)
+  "Wraps the specified region (or the current 'symbol / word'
+     with some textual markers that this function requests from the
+     user. Opening-type text, like parens and angle-brackets will
+     insert the matching closing symbol.
+
+     This function also supports some org-mode wrappers:
+
+       - `#s` wraps the region in a source code block
+       - `#e` wraps it in an example block
+       - `#q` wraps it in an quote block"
+  (interactive "r\nsEnter text to surround: " start end txt)
+
+  ;; If the region is not active, we use the 'thing-at-point' function
+  ;; to get a "symbol" (often a variable or a single word in text),
+  ;; and use that as our region.
+
+  (if (not (region-active-p))
+      (let ((new-region (bounds-of-thing-at-point 'symbol)))
+        (setq start (car new-region))
+        (setq end (cdr new-region))))
+
+  ;; We create a table of "odd balls" where the front and the end are
+  ;; not the same string.
+  (let* ((s-table '(("#e" . ("#+BEGIN_EXAMPLE\n" "\n#+END_EXAMPLE") )
+                    ("#s" . ("#+BEGIN_SRC \n"    "\n#+END_SRC") )
+                    ("#q" . ("#+BEGIN_QUOTE\n"   "\n#+END_QUOTE"))
+                    ("<"  . ("<" ">"))
+                    ("("  . ("(" ")"))
+                    ("{"  . ("{" "}"))
+                    ("["  . ("[" "]"))))    ; Why yes, we'll add more
+         (s-pair (assoc-default txt s-table)))
+
+    ;; If txt doesn't match a table entry, then the pair will just be
+    ;; the text for both the front and the back...
+    (unless s-pair
+      (setq s-pair (list txt txt)))
+
+    (save-excursion
+      (narrow-to-region start end)
+      (goto-char (point-min))
+      (insert (car s-pair))
+      (goto-char (point-max))
+      (insert (cadr s-pair))
+      (widen))))
+
+(global-set-key (kbd "C-+") 'surround)
+
+(defun surround-text (txt)
+  (if (region-active-p)
+      (surround (region-beginning) (region-end) txt)
+    (surround nil nil txt)))
+
+(defun surround-text-with (surr-str)
+  "Returns an interactive function that when called, will surround the region (or word) with the SURR-STR string."
+  (lexical-let ((text surr-str))
+    (lambda ()
+      (interactive)
+      (surround-text text))))
